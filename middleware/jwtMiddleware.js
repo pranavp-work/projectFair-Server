afad399149445ffda21e2ea9b6cfa316c70a6651
pranavp-work <pranavp.work@gmail.com>
2025-01-20 21:03:21 +0530

    split method and index to return token --code added

--- middleware/jwtMiddleware.js
+++ middleware/jwtMiddleware.js
@@ -1,7 +1,7 @@
 const jwtMiddleware = (req, res, next) => {
     console.log('inside jwt Middleware');
 
-    const token = req.headers['authorization']
+    const token = req.headers['authorization'].split(' ')[1]
     console.log(token);
 
     next();
